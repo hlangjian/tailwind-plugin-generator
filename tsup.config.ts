8089ac19393d2a2ca3ccc34c0b6b22e2366930f4
hlangjian <huanglangjian@gmail.com>
2023-09-04 10:13:57 +0800

    change build setting

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-    entry: {
-        main: "generator.ts",
-        template: "template.ts"
+export default defineConfig([
+    {
+        entry: {
+            main: "generator.ts",
+        },
+        splitting: false,
+        sourcemap: true,
+        clean: true,
+        format: ['esm'],
+        dts: true
     },
-    splitting: false,
-    sourcemap: true,
-    clean: true,
-    format: ['esm'],
-    dts: true
-})
+    {
+        entry: {
+            template: "template.ts"
+        },
+        splitting: false,
+        clean: true,
+        format: ['esm'],
+        dts: true
+    }
+])
